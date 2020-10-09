7982297da9d6fc609d3898123411a45dce40f7b4
dstark <gorokhov.dv93@gmail.com>
2020-10-09 14:23:13 +0300

    add new fetch booking

--- src/rooms.js
+++ src/rooms.js
@@ -1,14 +1,18 @@
 const URL_rooms_name_db = 'http://localhost:3000/room'
+const URL_booking_room = 'http://localhost:3000/booking'
+let booking = []
+let rooms = []
+let roomID = null
 
 $().ready(() => {
+    hideCalendar()
     let pick_date = $('.datepicker-here')
     getRooms_db()
     pick_date.datepicker({
         minDate: new Date()
     })
     pick_date.on('click', function () {
-        $('#see_date').text(pick_date.val())
-        console.log(pick_date.val());
+        $('#see_date').text('')
         show_time()
     })
 });
@@ -17,13 +21,23 @@ function getRooms_db() {
     fetch(URL_rooms_name_db).then(response => {
         return response.json()
     }).then(data => {
-        data.forEach((elem, index) => {
-            let allRooms = $('<button class="card-text btn-block">')
-            allRooms.attr('index', index)
+        console.log(data);
+        rooms = data
+        rooms.forEach((elem, index) => {
+            let allRooms = $('<button class="card-text btn-block btn_room">')
+            allRooms.attr('roomID', elem.id)
             $('#user_room').append(allRooms)
-            allRooms.text(data[index].roomName)
+            allRooms.text(rooms[index].roomName)
+            console.log(elem.roomName);
         });
+        $('.btn_room').on('click', function () {
+            show_calendar(this.getAttribute('roomID'), this.textContent)
+        })
     })
+}
+
+function hideCalendar() {
+    $('.datepicker-here').addClass('d-none')
 }
 
 function show_time() {
@@ -61,12 +75,24 @@ function show_time() {
             pref_m = '0'
         }
         let item_time = $('<div>')
+        item_time.addClass('mt-3')
         item_time.text(pref + h + ':' + pref_m + m)
         let item_booking = $('<button>')
-        item_booking.addClass('ml-5 mb-1')
+        item_booking.addClass('booking btn btn-primary btn-sm ml-5 mb-1')
         item_booking.text('booking')
         $('#see_date').append(item_time)
         $(item_time).append(item_booking)
-        console.log(d.toLocaleDateString());
     }
+}
+
+function show_calendar(id, text) {
+    $('.room_name').text(text)
+    roomID = id
+    $('.datepicker-here').removeClass('d-none')
+    fetch(URL_booking_room + '?roomId=' + roomID).then(response => {
+        return response.json()
+    }).then(data => {
+        booking = data
+        console.log(booking);
+    })
 }
